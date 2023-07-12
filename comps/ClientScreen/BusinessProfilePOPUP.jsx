8e083ec8824581040b5fc721c58ca9707e86bad0
InbarNagar <noabok27@gmail.com>
2023-07-12 20:10:04 +0300

    inbR

--- comps/ClientScreen/BusinessProfilePOPUP.jsx
+++ comps/ClientScreen/BusinessProfilePOPUP.jsx
@@ -7,6 +7,7 @@ import { useFocusEffect } from '@react-navigation/native';
 import { UserContext } from "../UserDietails";
 import { useNavigation} from "@react-navigation/core";
 import Header from "../obj/Header";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 
@@ -37,16 +38,16 @@ let onTimeSum = 0;
         let cleanlinessSum = 0;
         let overallRatingSum = 0;
 
-const handleInstagramLink = async () => {
-  try {
-    // const url = 'https://www.instagram.com/your_instagram_account';
-    const url = `https://www.instagram.com/${businessDetails.Instagram_link}`;
-    await Linking.openURL(url);
-    console.log(Linking.openURL(url))
-  } catch (error) {
-    console.error('שגיאה בפתיחת האינסטגרם:', error);
-  }
-};
+// const handleInstagramLink = async () => {
+//   try {
+//     // const url = 'https://www.instagram.com/your_instagram_account';
+//     const url = `https://www.instagram.com/${businessDetails.Instagram_link}`;
+//     await Linking.openURL(url);
+//     console.log(Linking.openURL(url))
+//   } catch (error) {
+//     console.error('שגיאה בפתיחת האינסטגרם:', error);
+//   }
+// };
 
 
 
@@ -225,35 +226,76 @@ const handleInstagramLink = async () => {
   <AntDesign name="downcircleo" size={24} color="black" />
   </View>
   </TouchableOpacity>
-  {ShowReviewsSection&&businessRankArr.length>0&&
-  <ScrollView>
-  {businessRankArr.map((bus) => {
-              console.log("key: "+bus.Number_appointment);
-              return (
-                <>
-                <View style={styles.card}>
-  <View style={styles.row}>
-    <Text style={styles.titleCard}>ציון כללי: {bus.Overall_rating}</Text>
-  </View>
-  <View style={styles.row}>
-    <Text style={styles.rightText}>ניקיון: {bus.Cleanliness}</Text>
-    <Text style={styles.rightText}>זמנים: {bus.On_time}</Text>
-    <Text style={styles.rightText}>מקצועיות: {bus.Professionalism}</Text>
-  </View>
-  <View style={styles.customerCommentContainer}>
-    <Text style={styles.titleCard} >תגובת הלקוח: </Text>
-    <Text style={styles.customerComment}>{bus.Comment}</Text>
-  </View>
-</View>
-</>
 
-               
-              );
-            })}
-  </ScrollView>}
-</View>
+  
+  {ShowReviewsSection&&businessRankArr.length>0&&
+      <ScrollView>
+        <View style={styles.container}>
+      {businessRankArr.map((bus) => {
+        console.log("key: " + bus.Number_appointment);
+        return (
+          <View key={bus.Number_appointment} style={styles.card}>
+          <View style={styles.reviewRow}>
+            <MaterialCommunityIcons
+              name="star"
+              size={24}
+              color="gold"
+              style={styles.icon}
+            />
+            <Text style={styles.ratingTitle}>ציון כללי: {bus.Overall_rating}</Text>
+          </View>
+          <View style={styles.parameterRow}>
+            <View style={styles.parameterContainer}>
+              <MaterialCommunityIcons
+                name="broom"
+                size={24}
+                color="gray"
+                style={styles.parameterIcon}
+              />
+              <Text style={styles.parameterText}>ניקיון:</Text>
+              <Text style={styles.parameterValue}>{bus.Cleanliness}</Text>
+            </View>
+            <View style={styles.parameterContainer}>
+              <MaterialCommunityIcons
+                name="timer"
+                size={24}
+                color="gray"
+                style={styles.parameterIcon}
+              />
+              <Text style={styles.parameterText}>זמנים:</Text>
+              <Text style={styles.parameterValue}>{bus.On_time}</Text>
+            </View>
+            <View style={styles.parameterContainer}>
+              <MaterialCommunityIcons
+                name="briefcase"
+                size={24}
+                color="gray"
+                style={styles.parameterIcon}
+              />
+              <Text style={styles.parameterText}>מקצועיות:</Text>
+              <Text style={styles.parameterValue}>{bus.Professionalism}</Text>
+            </View>
+          </View>
+          <View style={styles.customerCommentContainer}>
+            <MaterialCommunityIcons
+              name="comment"
+              size={24}
+              color="gray"
+              style={styles.parameterIcon}
+            />
+            <Text style={styles.customerComment}>"{bus.Comment}"</Text>
+          </View>
+        </View>
+        );
+      })}
+      </View>
+    </ScrollView>
+}
+  </View>
+
+
           </ScrollView>
-          <Button title="סגור" onPress={onClose} />
+          <Button title="סגור" onPress={onClose} color={"rgb(92, 71, 205)"} />
 
         </View>
       </View>
@@ -279,7 +321,6 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
-    
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
@@ -287,12 +328,7 @@ const styles = StyleSheet.create({
     // height: "80%",
     // maxWidth: 500,
     },
-  // scrollContent: {
-  //   // flexGrow: 1,
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   width: "90%",
-  // },
+
   scrollView: {
     width: '100%', // קביעת רוחב התוכן ל-100% של הכרטיסייה
   },
@@ -327,17 +363,8 @@ view1:{
     flexDirection:'column',
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'fff',
     
 },
-  // card: {
-  //   borderWidth: 1,
-  //   borderRadius: 10,
-  //   borderColor: '#d3d3d3',
-  //   padding: 10,
-  //   marginVertical: 5,
-  //   backgroundColor: "grey",
-  // },
   title: {
     fontWeight: 'bold',
     marginBottom: 5,
@@ -372,7 +399,6 @@ view1:{
   link: {
     padding: 10, // Add padding to increase touchable area
   },
-  
   imageInScrollView: {
     width: 100,
     height: 100,
@@ -411,40 +437,58 @@ view1:{
   reviewItem: {
     marginBottom: 10,
   },
-
-
+  container: {
+    padding: -90,
+    backgroundColor: "#fff",
+  },
   card: {
-    borderWidth: 3,
+    backgroundColor: "#f2f2f2",
     borderRadius: 10,
-    borderColor: 'rgb(92, 71, 205)',
-    padding: 5,
-    marginVertical: 5,
-    width:'100%',
+    marginBottom: 20,
+    padding: 0,
+  
+  },
+  reviewRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  
+  ratingTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
     
-    // backgroundColor: '#e6e6fa',
   },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  parameterRow: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginBottom:0,
   },
-  titleCard: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    flex: 1,
-    textAlign: 'center',
-    marginBottom: 10
+  parameterContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 10,
   },
-  rightText: {
-    textAlign: 'center',
-    marginBottom: 10
+  parameterIcon: {
+    marginRight: 5,
+  },
+  parameterText: {
+    color: "#666",
+    textAlign:'left'
+  },
+  parameterValue: {
+    color: "#333",
+    fontWeight: "bold",
+    margin:6,
+    marginLeft:-29
   },
   customerCommentContainer: {
-    alignItems: 'center',
-    marginTop: 10,
+    flexDirection: "row"
   },
   customerComment: {
-    textAlign: 'center',
-    fontSize: 20
+    color: "#666",
+    textAlign:'left'
   },
 
 });
